bfb57e43ac997637c80c1c2439fbba15a544c7d5
PipS <PipS@DESKTOP-FRMKOBB>
2021-01-12 09:56:56 +0000

    API ProductsController

---
+++ Store.Web/Controllers/API/ProductsControllercs.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace Store.Web.Controllers.API
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Store.Web.Data;
+
+    [Route("api/[Controller]")]
+    public class ProductsControllercs : Controller
+    {
+        private readonly IProductRepository productRepository;
+
+        public ProductsControllercs(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok(this.productRepository.GetAll());
+        }    
+    }
+}
